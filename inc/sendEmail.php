ee5904830c8ad6c0e24defa1849b08e9795af8af
dnmJustMe <dan.nm9906@gmail.com>
2023-03-22 15:44:33 -0400

    adding send mail

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: * ');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('content-type: application/json; charset=utf-8');
 
 // Replace this with your own email address
 $siteOwnersEmail = 'daniel.navarro@ihatuey.cu';
