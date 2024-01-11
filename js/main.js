f4fb25f4c306153dc5e0e31000d11685dee55997
DNMjustMe <dan.nm9906@gmail.com>
2024-01-11 17:35:02 -0500

    updating languaje json

--- js/main.js
+++ js/main.js
@@ -1,318 +1,345 @@
-/** 
+/**
  * ===================================================================
  * main js
  *
- * ------------------------------------------------------------------- 
+ * -------------------------------------------------------------------
  */
 
 (function ($) {
+  "use strict";
 
-	"use strict";
-
-	/*---------------------------------------------------- */
-	/* Preloader
+  /*---------------------------------------------------- */
+  /* Preloader
 	------------------------------------------------------ */
-	$(window).load(function () {
+  $(window).load(function () {
+    // will first fade out the loading animation
+    $("#loader").fadeOut("slow", function () {
+      // will fade out the whole DIV that covers the website.
+      $("#preloader").delay(300).fadeOut("slow");
+    });
+  });
 
-		// will first fade out the loading animation 
-		$("#loader").fadeOut("slow", function () {
-
-			// will fade out the whole DIV that covers the website.
-			$("#preloader").delay(300).fadeOut("slow");
-
-		});
-
-	})
-
-
-	/*---------------------------------------------------- */
-	/* FitText Settings
+  /*---------------------------------------------------- */
+  /* FitText Settings
 	------------------------------------------------------ */
-	setTimeout(function () {
+  setTimeout(function () {
+    $("#intro h1").fitText(1, { minFontSize: "42px", maxFontSize: "84px" });
+  }, 100);
 
-		$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
-
-	}, 100);
-
-
-	/*---------------------------------------------------- */
-	/* FitVids
+  /*---------------------------------------------------- */
+  /* FitVids
 	------------------------------------------------------ */
-	$(".fluid-video-wrapper").fitVids();
+  $(".fluid-video-wrapper").fitVids();
 
-
-	/*---------------------------------------------------- */
-	/* Owl Carousel
+  /*---------------------------------------------------- */
+  /* Owl Carousel
 	------------------------------------------------------ */
-	$("#owl-slider").owlCarousel({
-		navigation: false,
-		pagination: true,
-		itemsCustom: [
-			[0, 1],
-			[700, 2],
-			[960, 3]
-		],
-		navigationText: false
-	});
+  $("#owl-slider").owlCarousel({
+    navigation: false,
+    pagination: true,
+    itemsCustom: [
+      [0, 1],
+      [700, 2],
+      [960, 3],
+    ],
+    navigationText: false,
+  });
 
-
-	/*----------------------------------------------------- */
-	/* Alert Boxes
+  /*----------------------------------------------------- */
+  /* Alert Boxes
 		------------------------------------------------------- */
-	$('.alert-box').on('click', '.close', function () {
-		$(this).parent().fadeOut(500);
-	});
+  $(".alert-box").on("click", ".close", function () {
+    $(this).parent().fadeOut(500);
+  });
 
-
-
-	/*---------------------------------------------------- */
-	/*	Masonry
+  /*---------------------------------------------------- */
+  /*	Masonry
 	------------------------------------------------------ */
-	var containerProjects = $('#folio-wrapper');
+  var containerProjects = $("#folio-wrapper");
 
-	containerProjects.imagesLoaded(function () {
+  containerProjects.imagesLoaded(function () {
+    containerProjects.masonry({
+      itemSelector: ".folio-item",
+      resize: true,
+    });
+  });
 
-		containerProjects.masonry({
-			itemSelector: '.folio-item',
-			resize: true
-		});
-
-	});
-
-
-	/*----------------------------------------------------*/
-	/*	Modal Popup
+  /*----------------------------------------------------*/
+  /*	Modal Popup
 	------------------------------------------------------*/
-	$('.item-wrap a').magnificPopup({
+  $(".item-wrap a").magnificPopup({
+    type: "inline",
+    fixedContentPos: false,
+    removalDelay: 300,
+    showCloseBtn: false,
+    mainClass: "mfp-fade",
+  });
 
-		type: 'inline',
-		fixedContentPos: false,
-		removalDelay: 300,
-		showCloseBtn: false,
-		mainClass: 'mfp-fade'
+  $(document).on("click", ".popup-modal-dismiss", function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
 
-	});
-
-	$(document).on('click', '.popup-modal-dismiss', function (e) {
-		e.preventDefault();
-		$.magnificPopup.close();
-	});
-
-
-	/*-----------------------------------------------------*/
-	/* Navigation Menu
+  /*-----------------------------------------------------*/
+  /* Navigation Menu
 ------------------------------------------------------ */
-	var toggleButton = $('.menu-toggle'),
-		nav = $('.main-navigation');
+  var toggleButton = $(".menu-toggle"),
+    nav = $(".main-navigation");
 
-	// toggle button
-	toggleButton.on('click', function (e) {
+  // toggle button
+  toggleButton.on("click", function (e) {
+    e.preventDefault();
+    toggleButton.toggleClass("is-clicked");
+    nav.slideToggle();
+  });
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
+  // nav items
+  nav.find("li a").on("click", function () {
+    // update the toggle button
+    toggleButton.toggleClass("is-clicked");
+    // fadeout the navigation panel
+    nav.fadeOut();
+  });
 
-	});
-
-	// nav items
-	nav.find('li a').on("click", function () {
-
-		// update the toggle button 		
-		toggleButton.toggleClass('is-clicked');
-		// fadeout the navigation panel
-		nav.fadeOut();
-
-	});
-
-
-	/*---------------------------------------------------- */
-	/* Highlight the current section in the navigation bar
+  /*---------------------------------------------------- */
+  /* Highlight the current section in the navigation bar
 	------------------------------------------------------ */
-	var sections = $("section"),
-		navigation_links = $("#main-nav-wrap li a");
+  var sections = $("section"),
+    navigation_links = $("#main-nav-wrap li a");
 
-	sections.waypoint({
+  sections.waypoint({
+    handler: function (direction) {
+      var active_section;
 
-		handler: function (direction) {
+      active_section = $("section#" + this.element.id);
 
-			var active_section;
+      if (direction === "up") active_section = active_section.prev();
 
-			active_section = $('section#' + this.element.id);
+      var active_link = $(
+        '#main-nav-wrap a[href="#' + active_section.attr("id") + '"]'
+      );
 
-			if (direction === "up") active_section = active_section.prev();
+      navigation_links.parent().removeClass("current");
+      active_link.parent().addClass("current");
+    },
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
+    offset: "25%",
+  });
 
-			navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		},
-
-		offset: '25%'
-	});
-
-
-	/*---------------------------------------------------- */
-	/* Smooth Scrolling
+  /*---------------------------------------------------- */
+  /* Smooth Scrolling
 	------------------------------------------------------ */
-	$('.smoothscroll').on('click', function (e) {
+  $(".smoothscroll").on("click", function (e) {
+    e.preventDefault();
 
-		e.preventDefault();
+    var target = this.hash,
+      $target = $(target);
 
-		var target = this.hash,
-			$target = $(target);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        800,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  });
 
-		$('html, body').stop().animate({
-			'scrollTop': $target.offset().top
-		}, 800, 'swing', function () {
-			window.location.hash = target;
-		});
-
-	});
-
-
-	/*---------------------------------------------------- */
-	/*  Placeholder Plugin Settings
+  /*---------------------------------------------------- */
+  /*  Placeholder Plugin Settings
 	------------------------------------------------------ */
-	$('input, textarea, select').placeholder()
+  $("input, textarea, select").placeholder();
 
-
-	/*---------------------------------------------------- */
-	/*	contact form
+  /*---------------------------------------------------- */
+  /*	contact form
 	------------------------------------------------------ */
 
-	/* local validation */
-	$('#contactForm').validate({
+  /* local validation */
+  $("#contactForm").validate({
+    /* submit via ajax */
+    submitHandler: function (form) {
+      var sLoader = $("#submit-loader");
 
-		/* submit via ajax */
-		submitHandler: function (form) {
+      $.ajax({
+        type: "POST",
+        url: "https://dnmenviarcorreo.000webhostapp.com/app/sendEmail.php",
+        data: $("#contactForm").serialize(),
+        beforeSend: function () {
+          sLoader.fadeIn();
+        },
+        success: function (msg) {
+          // Message was sent
+          if (msg == "OK") {
+            sLoader.fadeOut();
+            $("#message-warning").hide();
+            $("#message-success").fadeIn();
+            document.getElementById("contactForm").reset();
+            setTimeout(() => {
+              $("#message-success").fadeOut();
+            }, 5000);
+          }
+          // There was an error
+          else {
+            sLoader.fadeOut();
+            $("#message-warning").html(msg);
+            $("#message-warning").fadeIn();
+            setTimeout(() => {
+              $("#message-warning").fadeOut();
+            }, 5000);
+          }
+          console.log(msg["responseText"]);
+        },
+        error: function (msg) {
+          sLoader.fadeOut();
+          if (msg["responseText"] == "OK") {
+            sLoader.fadeOut();
+            $("#message-warning").hide();
+            $("#message-success").fadeIn();
+            document.getElementById("contactForm").reset();
+            setTimeout(() => {
+              $("#message-success").fadeOut();
+            }, 5000);
+          }
+          // There was an error
+          else {
+            sLoader.fadeOut();
+            $("#message-warning").html(msg["responseText"]);
+            $("#message-warning").fadeIn();
+            setTimeout(() => {
+              $("#message-warning").fadeOut();
+            }, 5000);
+          }
+          console.log(msg["responseText"]);
+        },
+      });
+    },
+  });
 
-			var sLoader = $('#submit-loader');
-
-			$.ajax({
-
-				type: "POST",
-				url: "https://dnmenviarcorreo.000webhostapp.com/app/sendEmail.php",
-				data: $('#contactForm').serialize(),
-				beforeSend: function () {
-
-					sLoader.fadeIn();
-
-				},
-				success: function (msg) {
-
-					// Message was sent
-					if (msg == 'OK') {
-						sLoader.fadeOut();
-						$('#message-warning').hide();
-						$('#message-success').fadeIn();
-						document.getElementById('contactForm').reset()
-						setTimeout(() => {
-							$('#message-success').fadeOut();
-						}, 5000);
-					}
-					// There was an error
-					else {
-						sLoader.fadeOut();
-						$('#message-warning').html(msg);
-						$('#message-warning').fadeIn();
-						setTimeout(() => {
-							$('#message-warning').fadeOut();
-						}, 5000);
-					}
-					console.log(msg['responseText'])
-
-				},
-				error: function (msg) {
-
-					sLoader.fadeOut();
-					if (msg['responseText'] == 'OK') {
-						sLoader.fadeOut();
-						$('#message-warning').hide();
-						$('#message-success').fadeIn();
-						document.getElementById('contactForm').reset()
-						setTimeout(() => {
-							$('#message-success').fadeOut();
-						}, 5000);
-					}
-					// There was an error
-					else {
-						sLoader.fadeOut();
-						$('#message-warning').html(msg['responseText']);
-						$('#message-warning').fadeIn();
-						setTimeout(() => {
-							$('#message-warning').fadeOut();
-						}, 5000);
-					}
-					console.log(msg['responseText'])
-
-				}
-
-			});
-		}
-
-	});
-
-
-	/*----------------------------------------------------- */
-	/* Back to top
+  /*----------------------------------------------------- */
+  /* Back to top
 ------------------------------------------------------- */
-	var pxShow = 300; // height on which the button will show
-	var fadeInTime = 400; // how slow/fast you want the button to show
-	var fadeOutTime = 400; // how slow/fast you want the button to hide
-	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+  var pxShow = 300; // height on which the button will show
+  var fadeInTime = 400; // how slow/fast you want the button to show
+  var fadeOutTime = 400; // how slow/fast you want the button to hide
+  var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
-	// Show or hide the sticky footer button
-	jQuery(window).scroll(function () {
+  // Show or hide the sticky footer button
+  jQuery(window).scroll(function () {
+    if (!$("#header-search").hasClass("is-visible")) {
+      if (jQuery(window).scrollTop() >= pxShow) {
+        jQuery("#go-top").fadeIn(fadeInTime);
+      } else {
+        jQuery("#go-top").fadeOut(fadeOutTime);
+      }
+    }
+  });
 
-		if (!($("#header-search").hasClass('is-visible'))) {
+  $("#contratar-service1").click(function () {
+    autocompletar("Desarrollo web");
+  });
 
-			if (jQuery(window).scrollTop() >= pxShow) {
-				jQuery("#go-top").fadeIn(fadeInTime);
-			} else {
-				jQuery("#go-top").fadeOut(fadeOutTime);
-			}
+  $("#contratar-service2").click(function () {
+    autocompletar("Aplicaciones web");
+  });
 
-		}
+  $("#contratar-service3").click(function () {
+    autocompletar("Soluciones informáticas");
+  });
 
-	});
+  $("#contratar-service4").click(function () {
+    autocompletar("Soporte técnico");
+  });
 
-	$('#contratar-service1').click(function () {
-		autocompletar("Desarrollo web")
-	})
+  $("#contratar-service5").click(function () {
+    autocompletar("Mantenimiento");
+  });
 
-	$('#contratar-service2').click(function () {
-		autocompletar("Aplicaciones web")
-	})
+  function autocompletar(campo) {
+    $("#contactSubject").val("");
+    $("#contactMessage").val("");
+    $("#contactSubject").attr(
+      "placeholder",
+      `Asunto: ${campo}. (Presione 'Espacio' para autocompletar)`
+    );
+    $("#contactMessage").attr(
+      "placeholder",
+      `Mensaje: Hola, me gustaría contratar sus servicios de ${campo}. (Presione 'Espacio' para autocompletar)`
+    );
+    $("#contactName").focus();
+    $("#contactSubject").one("keyup", function (event) {
+      if (event.which == 32) {
+        $("#contactSubject").val(campo);
+      }
+    });
+    $("#contactMessage").one("keyup", function (event) {
+      if (event.which == 32) {
+        $("#contactMessage").val(
+          `Hola, me gustaría contratar sus servicios de ${campo}.`
+        );
+      }
+    });
+  }
 
-	$('#contratar-service3').click(function () {
-		autocompletar("Soluciones informáticas")
-	})
 
-	$('#contratar-service4').click(function () {
-		autocompletar("Soporte técnico")
-	})
+  async function change_languaje(lang) {
+    const URL = "../lang/" + lang + ".json";
 
-	$('#contratar-service5').click(function () {
-		autocompletar("Mantenimiento")
-	})
+    try {
+      const response = await fetch(URL);
 
-	function autocompletar(campo) {
-		$("#contactSubject").val("")
-		$("#contactMessage").val("")
-		$("#contactSubject").attr("placeholder", `Asunto: ${campo}. (Presione 'Espacio' para autocompletar)`)
-		$("#contactMessage").attr("placeholder", `Mensaje: Hola, me gustaría contratar sus servicios de ${campo}. (Presione 'Espacio' para autocompletar)`)
-		$("#contactName").focus()
-		$("#contactSubject").keyup(function (event) {
-			if (event.which == 32) {
-				$("#contactSubject").val(campo)
-			}
-		});
-		$("#contactMessage").keyup(function (event) {
-			if (event.which == 32) {
-				$("#contactMessage").val(`Hola, me gustaría contratar sus servicios de ${campo}.`)
-			}
-		});
-	}
+      if (!response.ok) {
+        throw new Error(
+          "Error al cargar el archivo JSON: " + response.statusText
+        );
+      }
 
+      const data = await response.json();
+
+      for (let clave in data) {
+        $("#" + clave).fadeOut(200, function () {
+          $("#" + clave).text(data[clave]).fadeIn(200);
+        })
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Obtén el valor de la variable en localStorage
+  var lang = window.localStorage.getItem("lang");
+
+  // Si la variable no existe, crea la variable en localStorage
+  if (lang === null) {
+    window.localStorage.setItem("lang", "es");
+    var lang = window.localStorage.getItem("lang");
+
+    $("#lang_checkbox").prop("checked", false);
+  } else {
+    if (lang == "es") {
+      $("#lang_checkbox").prop("checked", false);
+    } else {
+      $("#lang_checkbox").prop("checked", true);
+    }
+  }
+
+  $("#lang_checkbox").on("change", function () {
+    var valorRadio = $("#lang_checkbox").prop("checked");
+    if (valorRadio) {
+      lang = "en";
+    } else {
+      lang = "es";
+    }
+
+    window.localStorage.setItem("lang", lang);
+    var lang = window.localStorage.getItem("lang");
+    $("input[name='lang-switch'][value='" + lang + "']").prop("checked", true);
+
+    change_languaje(lang);
+  });
+
+  change_languaje(lang);
 })(jQuery);
--- lang/lang.js
+++
@@ -1,3 +0,0 @@
-const es = {
-    bienvenida : "Hola"
-}
